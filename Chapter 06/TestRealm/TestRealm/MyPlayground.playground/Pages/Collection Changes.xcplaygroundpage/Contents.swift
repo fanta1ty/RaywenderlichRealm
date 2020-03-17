596f2e89f11ee03c9e6624002ea887242d8dc28f
Thinh Nguyen <thinhnguyen12389@gmail.com>
2020-03-17 15:15:30 +0700

    Updated

---
+++ Chapter 06/TestRealm/TestRealm/MyPlayground.playground/Pages/Collection Changes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,49 @@
+//: # 🙎‍♂️💁‍♀️👶👨‍👨‍👦‍👦 Collection Changes
+//: [Home](Start) |
+//: [Previous](@previous) |
+//: [Next](@next)
+
+import RealmSwift
+import PlaygroundSupport
+
+Example.of("Collection Changes")
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+//: **Setup Realm and preload some data**
+let configuration = Realm.Configuration(inMemoryIdentifier: "TemporaryRealm")
+let realm = try! Realm(configuration: configuration)
+
+try! TestDataSet.create(in: realm)
+
+
+
+//: [Next](@next)
+/*:
+ Copyright (c) 2018 Razeware LLC
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+ distribute, sublicense, create a derivative work, and/or sell copies of the
+ Software in any work that is designed, intended, or marketed for pedagogical or
+ instructional purposes related to programming, coding, application development,
+ or information technology.  Permission for such use, copying, modification,
+ merger, publication, distribution, sublicensing, creation of derivative works,
+ or sale is expressly withheld.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
---
+++ Chapter 06/TestRealm/TestRealm/MyPlayground.playground/Pages/Start.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+/*:
+# Notifications and Reactive Apps
+# 🖥📱⌚️
+
+This section provides several asynchronous examples that work best
+when executed individually.
+
+Table of contents:
+
+- [Observing an Object](Observing%20an%20Object)
+- [Observing a Collection](Observing%20a%20Collection)
+- [Collection Changes](Collection%20Changes)
+- [Avoid notifications for given tokens](Avoid%20notifications%20for%20given%20tokens)
+- [Realm-wide notifcations](Realm-wide%20notifications)
+*/
+
